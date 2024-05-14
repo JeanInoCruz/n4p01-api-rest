214959c91ddae783be24c9e31d47f4445fcd6d76
Jean Paul  Inofuente Cruz <kiral626@gmail.com>
2024-05-13 22:01:35 -0500

    Added CORS

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import express, { json } from 'express'
 import { moviesRouter } from './routes/movies.js'
+import { corsMiddleware } from './middlewares/cors.js'
 
 const app = express()
 app.use(json())
+app.use(corsMiddleware())
 app.disable('x-powered-by')
 
 app.use('/movies', moviesRouter)
